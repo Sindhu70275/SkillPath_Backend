7d233737fcb7b5f901beb7f740932d7727ac3068
Sindhu70275 <r.jayasindhu30@gmail.com>
2026-02-22 17:11:12 +0530

    getskills and create a skill api's and cors setup

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 import app from "./src/app.js";
 import dotenv from "dotenv";
+import cors from "cors";
+
 import connectDB from "./src/config/db.js";
 import errorHandler from "./src/middleware/errorHandler.js";
+
+import skillRoutes from "./src/routes/skill.routes.js";
 
 dotenv.config();
 connectDB();
 
 const PORT = process.env.PORT || 3000;
-console.log(`PORT: ${PORT}`);
+
+const allowedOrigins = process.env.CORS_ORIGINS.split(",");
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
 
 app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.use("/api/skills", skillRoutes);
