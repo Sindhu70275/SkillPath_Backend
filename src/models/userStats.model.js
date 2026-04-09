2c41bc2f028d467dcc42bd22ac30db5037f5220b
Sindhu70275 <r.jayasindhu30@gmail.com>
2026-04-09 23:07:18 +0530

    user stats model plan

---
+++ src/models/userStats.model.js
@@ -0,0 +1,51 @@
+const userStatsSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    // Daily streaks
+    currentDailyStreak: {
+      type: Number,
+      default: 0,
+    },
+    longestDailyStreak: {
+      type: Number,
+      default: 0,
+    },
+    lastActiveDate: {
+      type: Date,
+    },
+
+    // Weekly goals
+    weeklyGoal: {
+      type: Number,
+      default: 0,
+    },
+    weeklyCompleted: {
+      type: Number,
+      default: 0,
+    },
+    currentWeeklyStreak: {
+      type: Number,
+      default: 0,
+    },
+    longestWeeklyStreak: {
+      type: Number,
+      default: 0,
+    },
+    goalStartDate: {
+      type: Date,
+    },
+    goalEndDate: {
+      type: Date,
+    },
+  },
+  { timestamps: true },
+);
+
+const UserStats = mongoose.model("UserStats", userStatsSchema);
+
+export default UserStats;
